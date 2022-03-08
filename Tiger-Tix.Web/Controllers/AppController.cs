773b365262133a51f5e0917555e48c16a3f18639
JulianChastain <julian.g.chastain@gmail.com>
2022-03-08 12:05:52 -0500

    First commit

---
+++ Tiger-Tix.Web/Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiger_Tix.Web.ViewModels;
+
+namespace Tiger_Tix.Web
+{
+    public class AppController : Controller
+    {
+        // [HttpPost("/")]
+        public IActionResult Index(IndexViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Tiger-Tix.Web/ViewModels/IndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Tiger_Tix.Web.ViewModels
+{
+    public class IndexViewModel
+    {
+        public string UserName { get; set; }
+    }
+}
